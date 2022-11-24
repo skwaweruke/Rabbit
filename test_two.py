31a7cde2afd068257ecad8747ee1cc061b6d162c
skwaweru254 <skwaweru254@gmail.com>
2022-11-24 16:55:45 +0300

    eval and input functions

--- test_two.py
+++ test_two.py
@@ -1,6 +1,14 @@
 #   use eval function to accept only number.
 #   getting input from user using input function
 
-name = eval(input("Enter your name: "))
+name = input("Enter your name: ")
+
+#   name = eval(input("Enter your name: "))
+
 
 print("You typed", name)
+
+if name == "simon":
+    print("You entered a text.")
+if name != "simon":
+    print("Your input is a number.")
