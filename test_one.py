e5ad1ff3c28a7ae47c3a0baeccdfaaf083ca115f
skwaweru254 <skwaweru254@gmail.com>
2022-11-24 09:10:49 +0300

    Default boolean value

--- test_one.py
+++ test_one.py
@@ -1,3 +1,9 @@
 #   python test one
 
 print(bool(False) + 8 + bool(True))  # boolean value for true and false are 1 and 0 respectively
+
+print(bool(False)+1)
+
+print(bool(True)+1)
+
+print(bool('')+1)
