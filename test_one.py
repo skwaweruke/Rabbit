6be73e92ac847a4bccf6190e6e11bc75277257bb
skwaweru254 <skwaweru254@gmail.com>
2023-01-20 09:43:22 +0300

    We are here

--- test_one.py
+++ test_one.py
@@ -1,5 +1,5 @@
-#   python test one
-
+#   Python test one
+#   Python is really fun
 print(bool(False) + 8 + bool(True))  # boolean value for true and false are 1 and 0 respectively
 
 print(bool(False)+1)
